1232a929e925f853fef7a0105822e7477230d1c5
felipe reusch <felipereusch@MacBook-Air-de-felipe.local>
2021-11-16 11:25:12 -0300

    check

--- Felipe-Reusch-Avaliacao1/my-app/src/components/Header/index.js
+++ Felipe-Reusch-Avaliacao1/my-app/src/components/Header/index.js
@@ -6,14 +6,14 @@ const Header = ({ empresa, temaEscuro }) => {
     <header className="sticky-top">
       <nav className={`navbar navbar-expand-sm navbar-${temaEscuro ? 'dark' : 'light'} bg-${temaEscuro ? 'dark' : 'light'}`} aria-label="Third navbar example">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">{empresa}</a>
+          <a className="navbar-brand" href="#galeria">{empresa}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-end" id="navbarsExample03">
             <ul className="navbar-nav mb-2 mb-sm-0">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">Churrasco</a>
+                <a className="nav-link" aria-current="page" href="#galeria">Churrasco</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#galeria">Video</a>
